d0651a8ff44cac327593e7b965f9c7729c58f43e
khanhtran <khanhkhanh26797@gmail.com>
2020-08-18 12:25:54 +0700

    make it better

--- app.js
+++ app.js
@@ -15,16 +15,16 @@ app.set('view engine', 'hbs');
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb+srv://dbDemo:Khanh260797@cluster0.zzpze.mongodb.net/ToyStore";
 
-app.get('/', async function (req, res) {
-    let client = await MongoClient.connect(url);
-    let dbo = client.db("ToyStore");
-    let result = await dbo.collection("Users").find({}).toArray();
-    let product = await dbo.collection("Products").find({}).toArray();
-    let category = await dbo.collection("Category").find({}).toArray();
-    console.log(result)
-    console.log(product)
-    res.render('index', { model: result, list: product, category: category });
-})
+// app.get('/', async function (req, res) {
+//     let client = await MongoClient.connect(url);
+//     let dbo = client.db("ToyStore");
+//     let result = await dbo.collection("Users").find({}).toArray();
+//     let product = await dbo.collection("Products").find({}).toArray();
+//     let category = await dbo.collection("Category").find({}).toArray();
+//     console.log(result)
+//     console.log(product)
+//     res.render('index', { model: result, list: product, category: category });
+// })
 
 // app.get('/category', (req, res) => {
 //     res.render('category');
