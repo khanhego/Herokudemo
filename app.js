0364324c2d00d4e28bfbec7d2ad9cdf99c29535c
khanhtran <khanhkhanh26797@gmail.com>
2020-08-19 23:35:12 +0700

    make it better

--- app.js
+++ app.js
@@ -27,6 +27,13 @@ app.get('/', async function (req, res) {
     res.render('index', { model: result, list: product, category: category });
 })
 
+app.get('/listProduct', async function (req, res) {
+    let client = await MongoClient.connect(url);
+    let dbo = client.db("ToyStore");
+    let product = await dbo.collection("Products").find({}).toArray();
+    console.log(product)
+    res.render('listProduct', { list: product });
+})
 app.get('/category', (req, res) => {
     res.render('category');
 })
@@ -65,8 +72,21 @@ app.get('/filterCategory', async function (req, res) {
     let client = await MongoClient.connect(url);
     let dbo = client.db("ToyStore");
     let result = await dbo.collection("Products").find({ category: name }).toArray();
-    console.log("data filter12312412412", result)
+    console.log("data ", result)
     res.render('showFilter', { model: result });
+})
+// app.get('/search', function (req, res) {
+//     // res.render();
+// })
+app.get('/search', async function (req, res) {
+    let info= req.query.info;
+    //var ObjectID = require('mongodb').ObjectID;
+    let client = await MongoClient.connect(url);
+    let dbo = client.db("ToyStore");
+    let result=await dbo.collection("Products").find({name: new RegExp("^"+info,'i')}).toArray();
+    // let result = await dbo.collection("Products").findOne({name : /.*d.*/i});;
+    console.log("data search123", result)
+    res.render('listProduct', { list: result });
 })
 
 app.post('/doRegister', async (req, res) => {
@@ -101,13 +121,14 @@ app.get('/updateProduct', async (req, res) => {
     console.log("khanh query", req.query)
     console.log("khanh body", id)
     var ObjectID = require('mongodb').ObjectID;
-    let inputName = req.query.txtName;
+    let inputName = req.query.name;
     let price = req.query.price;
+    let category = req.query.category;
     console.log("text", inputName)
-    let updateInfo = { name: inputName };
+    let updateInfo = { name: inputName, price:price, category:category};
     let client = await MongoClient.connect(url);
     let dbo = client.db("ToyStore");
-    await dbo.collection("Products").updateOne({ _id: ObjectID(id) }, { $set: { name: inputName } });
+    await dbo.collection("Products").updateOne({ _id: ObjectID(id) }, { $set: updateInfo });
     console.log('1234556')
     res.redirect('/');
 
@@ -157,6 +178,6 @@ app.get('/remove', async (req, res) => {
 
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT);
 
